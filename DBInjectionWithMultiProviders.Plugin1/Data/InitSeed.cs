bdc8aed50d02f21469180ec7ab39cd3ffc99462a
Alok Sharma <aloksharma2k27@gmail.com>
2021-03-15 15:22:48 +0530

    added seed intializer

---
+++ DBInjectionWithMultiProviders.Plugin1/Data/InitSeed.cs
@@ -0,0 +1,34 @@
+﻿using DBInjectionWithMultiProviders.Domain.Entities;
+using DBInjectionWithMultiProviders.Persistence.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBInjectionWithMultiProviders.Plugin1.Data
+{
+    public class InitSeed
+    {
+
+        public static void Initialize(PluginDBContext context)
+        {
+            context.UpsertSeed(
+            p => p.UserName,
+            p => new
+            {
+                p.IsActive,
+                p.FullName,
+                p.Email
+            },
+            new UserInfo
+            {
+                IsActive = true,
+                FullName = "My user name",
+                UserName = "ThisUser",
+                Email = "my@email",
+            }
+           );
+        }
+    }
+}
